91314b36c9917aadaae0d3b7090351ce386a2329
mrPomido <mihailpetrov29022000@gmail.com>
2024-10-03 23:16:50 +0700

    Windows + Linux socket init

---
+++ src/headers.h
@@ -0,0 +1,17 @@
+#ifdef _WIN32
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#endif
+#ifndef _WIN32 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#endif
